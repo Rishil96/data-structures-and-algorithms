c003649cb9f4dad4a2edc7dbb1cca93373a35a1c
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-07-19 12:07:11 +0530

    Add : Combination Sum 2 solution

--- 05-Backtracking/problems-11-to-15.cpp
+++ 05-Backtracking/problems-11-to-15.cpp
@@ -38,7 +38,45 @@ vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
 }
 
 
-// 12.
+// 12. Combination Sum 2
+void getCombiII(vector<int>& candidates, int &target, vector<vector<int>>& ans, vector<int>& temp, int currIndex) {
+    // Base Case
+    if (target == 0) {
+        ans.push_back(temp);
+        return ;
+    }
+    if (target < 0 || currIndex >= candidates.size()) return ;
+
+    // Ek Case
+    for (int i=currIndex; i<candidates.size(); i++) {
+        // Don't start a new combination if current index and previous index had the same num
+        if (i > currIndex && candidates[i] == candidates[i-1]) continue;
+
+        // Include current element in combination
+        temp.push_back(candidates[i]);
+        target -= candidates[i];
+        currIndex++;
+        // Recursive call from next index to not include current element again
+        getCombiII(candidates, target, ans, temp, i+1);
+        // Backtrack to previous state
+        currIndex--;
+        target += candidates[i];
+        temp.pop_back();
+
+    }
+
+    return ;
+}
+
+vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+    sort(candidates.begin(), candidates.end());     // Sort candidates
+    vector<vector<int>> ans;                        // Store all combinations
+    vector<int> temp;                               // Store current combination
+    int index = 0;
+    getCombiII(candidates, target, ans, temp, index);
+
+    return ans;
+}
 
 
 // 13.
