a7869cfa173b00481e635139cb558fd65bda6249
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-07-26 12:19:33 +0530

    Add : Remove cycle from linked list and Sort linked list 0, 1 and 2s solutions

--- 06-Linked-List/problems-6-to-10.cpp
+++ 06-Linked-List/problems-6-to-10.cpp
@@ -10,8 +10,52 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+void print(ListNode* head) {
+    while (head) {
+        cout << head -> val << " ";
+        head = head -> next;
+    }
 
-// 6.
+    cout << endl;
+    return ;
+}
+
+
+// 6. Remove Cycle from linked list
+void removeCycle(ListNode* &head) {
+    // Step 1: Find if cycle is present
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    // Use slow fast approach to make slow and fast pointers meet
+    while (fast) {
+        fast = fast -> next;
+        if (fast) {
+            slow = slow -> next;
+            fast = fast -> next;
+        }
+        if (slow == fast) break;
+    }
+
+    // Case where cycle is not present, fast will reach NULL 
+    if (!fast) return ;
+
+    // Step 2: Reset slow to head and start traversing with both slow and fast once
+    slow = head;
+    // Use this to store previous fast pointer so that we can remove the next pointer of node just before starting point of loop
+    ListNode* tail = NULL;      
+    
+    while (slow != fast) {
+        slow = slow -> next;
+        tail = fast;
+        fast = fast -> next;
+    }
+
+    // Once slow and fast pointer meet we can remove the next pointer of tail to break the cycle
+    tail -> next = NULL;
+
+    return ;
+}
 
 
 // 7. Check if linked list is a palindrome
@@ -90,7 +134,74 @@ ListNode* deleteDuplicates(ListNode* head) {
 }
 
 
-// 9. 
+// 9. Sort 0s, 1s and 2s in Linked List
+ListNode* segregate(ListNode *head) {
+    
+    // Create head and tail nodes for each value 0, 1 and 2
+    ListNode* zeroHead = new ListNode(0);
+    ListNode* oneHead = new ListNode(1);
+    ListNode* twoHead = new ListNode(2);
+    
+    ListNode* zeroTail = zeroHead;
+    ListNode* oneTail = oneHead;
+    ListNode* twoTail = twoHead;
+    
+    // Use current pointer to traverse the linked list
+    ListNode* curr = head;
+    
+    // Separate out 0s, 1s and 2s into individual lists
+    while (curr) {
+        if (curr -> val == 0) {
+            zeroTail -> next = curr;
+            zeroTail = curr;
+            curr = curr -> next;
+            zeroTail -> next = NULL;
+        }
+        else if (curr -> val == 1) {
+            oneTail -> next = curr;
+            oneTail = curr;
+            curr = curr -> next;
+            oneTail -> next = NULL;
+        }
+        else {
+            twoTail -> next = curr;
+            twoTail = curr;
+            curr = curr -> next;
+            twoTail -> next = NULL;
+        }
+    }
+    
+    // Remove initial placeholder node that was used to smoothly add new nodes in tail
+    if (zeroHead == zeroTail) {
+        zeroHead = NULL;
+        zeroTail = NULL;
+    }
+    else zeroHead = zeroHead -> next;
+
+    if (oneHead == oneTail) {
+        oneHead = NULL;
+        oneTail = NULL;
+    }
+    else oneHead = oneHead -> next;
+    
+    if (twoHead == twoTail) {
+        twoHead = NULL;
+        twoTail = NULL;
+    }
+    else twoHead = twoHead -> next;
+    
+    // If One list exists then merge two head at the tail of one list or else add two list directly on onehead
+    if (oneHead) oneTail -> next = twoHead;
+    else oneHead = twoHead; 
+    
+    // If zero list exists then merge one head with zero tail or else add one head directly on zero head 
+    if (zeroHead) {
+        zeroTail -> next = oneHead;
+    }
+    else zeroHead = oneHead;
+    
+    return zeroHead;
+}
 
 
 // 10.
@@ -99,7 +210,17 @@ ListNode* deleteDuplicates(ListNode* head) {
 
 int main() {
 
-    
+    ListNode* head = new ListNode(1);
+    ListNode* tail = new ListNode(5);
+    head -> next = new ListNode(2);
+    head -> next -> next = new ListNode(3);
+    head -> next -> next -> next = new ListNode(4);
+    head -> next -> next -> next -> next = tail;
+    tail -> next = head -> next;
+
+    // print(head);         // This will run infinitely as loop exits in linked list
+    removeCycle(head);
+    print(head);
 
     return 0;
 }
