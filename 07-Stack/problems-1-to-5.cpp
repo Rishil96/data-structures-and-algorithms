b2b565ada2d19c85ae196d6bc8699332986e8a14
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-08-05 13:01:19 +0530

    Add : Valid Parentheses and Sort stack solutions

--- 07-Stack/problems-1-to-5.cpp
+++ 07-Stack/problems-1-to-5.cpp
@@ -32,10 +32,67 @@ void Reverse(stack<int> &St){
 }
 
 
-// 2. 
+// 2. Valid Parentheses
+bool isValid(string s) {
+    stack<char> par;
+    for (auto ch: s) {
+        // Opening Bracket Case: Add in stack
+        if (ch == '(' || ch == '[' || ch == '{') {
+            par.push(ch);
+        }
+        // Closing Bracket Case: Pop fro stack if brackets match else return false
+        else {
+            if (par.empty()) return false;
+            else if (ch == ')' && par.top() == '(') par.pop();
+            else if (ch == ']' && par.top() == '[') par.pop();
+            else if (ch == '}' && par.top() == '{') par.pop();
+            else return false;
+        }
+    }
+
+    // If stack is empty return true
+    return par.empty();
+}
 
 
-// 3.
+// 3. Sort a stack
+void insertSorted(stack<int> &s, int &element) {
+    // Base Case: If stack becomes empty, single element is sorted so add the element and return
+    if (s.empty()) {
+        s.push(element);
+        return ;
+    }
+
+    // Ek Case
+    int topElement = s.top();
+
+    // Place our element if it is greater than the top element
+    if (element > topElement) {
+        s.push(element);
+        return ;
+    }
+    // Else call recursion to find the correct location for our element
+    else {
+        s.pop();
+        insertSorted(s, element);
+        s.push(topElement);
+    }
+
+    return ;
+}
+
+void sortStack(stack<int> &s) {
+    // Base Case
+    if (s.empty()) return ;
+
+    // Ek Case
+    int element = s.top();  s.pop();
+    sortStack(s);
+
+    // Baaki Recursion 
+    insertSorted(s, element);
+    return ;
+}
 
 
 // 4.
