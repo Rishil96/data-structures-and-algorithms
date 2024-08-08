ea79c7f2b505f830cee5dcd0b9c62182b07d2631
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-08-08 12:17:17 +0530

    Add : Next greater node in linked list

--- 07-Stack/problems-6-to-10.cpp
+++ 07-Stack/problems-6-to-10.cpp
@@ -3,6 +3,14 @@
 #include <vector>
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 
 // 6. Longest Valid Parentheses
 int longestValidParentheses(string s) {
@@ -114,10 +122,69 @@ string removeDuplicates(string s) {
 }
 
 
-// 9. 
+// 9. Count Bracket Reversals
+int countRev (string s) { 
+    // Edge Case: Odd length of s can never be balanced
+    if (s.size() & 1) return -1;
+    
+    // Use stack and remove the valid parentheses from it
+    stack<char> par;
+    for (auto ch: s) {
+        if (ch == '{') par.push(ch);
+        else {
+            if (!par.empty() && par.top() == '{') par.pop();
+            else par.push(ch);
+        }
+    }
+    
+    // After above loop, only invalid parentheses are left so we swap them
+    int ans = 0;
+    while (par.size() > 1) {
+        // Get pairs of parentheses and fix them
+        char right = par.top(); par.pop();
+        char left = par.top(); par.pop();
+        
+        // If both brackets are equal, then one swap can fix them
+        if (left == right) ans++;
+        // If both brackets are opposite, then 2 swaps are needed
+        else if (left == '}' && right == '{') ans += 2;
+    }
+    
+    return ans;
+}
 
 
-// 10.
+// 10. Next greater node in linked list
+void getNLN(ListNode* head, stack<int>& st, vector<int>& ans) {
+    // Base Case
+    if (!head) return ;
+    
+    // Recursive Call: TO reach last node first then come back to get next greater node
+    getNLN(head -> next, st, ans);
+    
+    // Ek Case
+    // Keep removing next nodes for current node if it is smaller
+    while (!st.empty() && head -> val >= st.top()) st.pop();
+    
+    // If stack is not empty insert top element of stack in answer else insert 0
+    if (!st.empty()) ans.push_back(st.top());
+    else ans.push_back(0);
+
+    // Add current node to stack to be considered as next greater node for previous nodes
+    st.push(head -> val);
+
+    return ;
+}
+
+vector<int> nextLargerNodes(ListNode* head) {
+    vector<int> ans;                    // Answer variable
+    stack<int> st;                      // Store next greater elements in stack
+    st.push(0);                         // Push 0 initially to denote 0 as next greater element of last node
+    getNLN(head, st, ans);              // Recursive call
+    reverse(ans.begin(), ans.end());    // Reverse ans array as we are adding answers from last node to first
+
+    return ans;
+}
 
 
 int main() {
