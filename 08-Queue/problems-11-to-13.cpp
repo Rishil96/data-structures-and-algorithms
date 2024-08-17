49589eeb1a4a810da5fa470b46548b6bd83f6835
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-08-17 11:30:48 +0530

    Add : Circular game solution

--- 08-Queue/problems-11-to-13.cpp
+++ 08-Queue/problems-11-to-13.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 
-// 11.
+// 11. Find the winner of the circular game
+int findTheWinner(int n, int k) {
+    // Create a queue and add all friends from 1 to n in it
+    queue<int> friends;
+    for (int i=1; i<=n; i++) friends.push(i);
+
+    // Play the game till there are more than 1 friends left in the queue
+    while (friends.size() > 1) {
+        // Count k - 1 friends in queue from start
+        for (int i=1; i<k; i++) {
+            int currentFriend = friends.front(); friends.pop();
+            friends.push(currentFriend);
+        }
+        // Remove the kth friend
+        friends.pop();
+    }
+
+    return friends.front();
+}
 
 
 // 12.
