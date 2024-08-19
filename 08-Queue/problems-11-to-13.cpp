dc754962e83e9265708d50b108727ab355257d09
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-08-19 12:32:33 +0530

    Add : Reveal cards in increasing order and Spread the secret solutions

--- 08-Queue/problems-11-to-13.cpp
+++ 08-Queue/problems-11-to-13.cpp
@@ -24,11 +24,79 @@ int findTheWinner(int n, int k) {
 }
 
 
-// 12.
+// 12. Reveal cards in increasing order
+vector<int> deckRevealedIncreasing(vector<int>& deck) {
+    // Add all indexes in deque to be used to place cards in order
+    deque<int> dq;
+    for (int i=0; i<deck.size(); i++) dq.push_back(i);
+
+    // Answer array
+    vector<int> answer(deck.size(), 0);
+
+    // Sort cards in increasing order
+    sort(deck.begin(), deck.end());
+
+    // Loop through the deck to place cards
+    for (int i=0; i<deck.size(); i++) {
+        // Get front index
+        int index = dq.front(); dq.pop_front();
+
+        // Place current card on index
+        answer[index] = deck[i];
+
+        // Place new front index in deque to the back
+        dq.push_back(dq.front()); dq.pop_front();
+    }
+
+    return answer;
+}
 
 
-// 13.
+// 13. Number of people aware of the secret
+int peopleAwareOfSecret(int n, int delay, int forget) {
+    const int MOD = 1e9 + 7; 
+    int currActiveSpreaders = 0;        // Current active spreaders of secret
+    int ans = 1;                        // On day 1, 1 person knows the secret as per problem statement
 
+    // Add entries of new people that got to know the secret should be made 
+    queue<pair<int, int>> delayQ;       // Track number of people who are waiting for delay to be over
+    queue<pair<int, int>> forgetQ;      // Track number of people who have not forgot the secret yet
+
+    delayQ.push({1, 1});
+    forgetQ.push({1, 1});
+
+    for (int day=1; day<=n; day++) {
+        // Step 1: Check if anyone is going to forget the secret today
+        /* 
+        Get it from front of forget queue, subtract the people that forgets the secret today 
+        from current active spreaders and the answer 
+        */
+        if (!forgetQ.empty() && forgetQ.front().first + forget <= day) {
+            auto front = forgetQ.front(); forgetQ.pop();
+            int forgettingToday = front.second;
+            ans = (ans - forgettingToday + MOD) % MOD;
+            currActiveSpreaders = (currActiveSpreaders - forgettingToday + MOD) % MOD;
+        }
+
+        // Step 2: Check how many people are becoming active spreaders today
+        /*
+        Get the front of delay queue, if the delay ends today add the number of people to the current active spreaders
+        */
+        if (!delayQ.empty() && delayQ.front().first + delay <= day) {
+            auto front = delayQ.front(); delayQ.pop();
+            currActiveSpreaders = (currActiveSpreaders + front.second) % MOD;
+        }
+
+        // Step 3: Spread the secret by adding the current active spreaders to the answer and push the new people data in both queues
+        if (currActiveSpreaders > 0) {
+            ans = (ans + currActiveSpreaders) % MOD;
+            delayQ.push({day, currActiveSpreaders});
+            forgetQ.push({day, currActiveSpreaders});
+        }
+    }
+
+    return ans;
+}
 
 
 int main() {
