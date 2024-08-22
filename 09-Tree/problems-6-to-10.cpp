41ca42ac4b1044f4da9820b4b5fc52f5be6f7642
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-08-22 12:02:57 +0530

    Add : Construct Binary Tree from Inorder and Preorder and also from Inorder and postorder traversal solutions

--- 09-Tree/problems-6-to-10.cpp
+++ 09-Tree/problems-6-to-10.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
 
 struct Node
@@ -44,10 +56,83 @@ int kthAncestor(Node *root, int k, int node)
 }
 
 
-// 7.
-// 8.
-// 9.
+// 7. Construct Binary Tree from Inorder and Preorder Traversal
+TreeNode* btHelper(vector<int>& preorder, vector<int>& inorder, int &pi, int inStart, int inEnd, unordered_map<int, int> &mapping) {
+    // Base Case
+    if (pi >= preorder.size() || inStart > inEnd) return NULL;
+
+    // Ek Case
+    // Create current node
+    TreeNode* root = new TreeNode(preorder[pi]);
+
+    // Find current node in inorder array
+    int inIndex = mapping[preorder[pi]];
+
+    pi++;
+
+    // Recursive call for left and right nodes
+    root -> left = btHelper(preorder, inorder, pi, inStart, inIndex - 1, mapping);
+    root -> right = btHelper(preorder, inorder, pi, inIndex + 1, inEnd, mapping);
+
+    return root;
+}
+
+TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    int preOrderIndex = 0;                      // Index to create nodes in tree using preorder
+    // Start and end index for inorder array to check if current element is valid
+    int inStart = 0;                            
+    int inEnd = inorder.size() - 1;
+
+    // Create mapping of each element to its index in inorder array to save time
+    unordered_map<int, int> mapping;
+    for (int i=0; i<preorder.size(); i++) {
+        mapping[inorder[i]] = i;
+    }
+
+    return btHelper(preorder, inorder, preOrderIndex, inStart, inEnd, mapping);
+}
+
+
+// 8. Construct Binary Tree from Inorder and Postorder Traversal
+TreeNode* btHelper(vector<int>& inorder, vector<int>& postorder, int &pi, int inStart, int inEnd, unordered_map<int, int> &mapping) {
+    // Base Case
+    if (pi < 0 || inStart > inEnd) return NULL;
+
+    // Ek Case
+    // Create current node
+    TreeNode* root = new TreeNode(postorder[pi]);
+    int inIndex = mapping[postorder[pi]];
+
+    // Decrement index of postorder denoting one node being created
+    pi--;
+
+    // Build left and right subtrees of nodes
+    root -> right = btHelper(inorder, postorder, pi, inIndex + 1, inEnd, mapping);
+    root -> left = btHelper(inorder, postorder, pi, inStart, inIndex - 1, mapping);
+
+    return root;
+}
+
+TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+    // Index variables to keep track of nodes created and the position of next node i.e. left or right
+    int pi = postorder.size() - 1;
+    int inStart = 0;
+    int inEnd = inorder.size() - 1;
+
+    // Mapping of elements to its position in inorder array
+    unordered_map<int, int> mapping;
+    for (int i=0; i<inorder.size(); i++) mapping[inorder[i]] = i;
+
+    return btHelper(inorder, postorder, pi, inStart, inEnd, mapping);
+}
+
+
+// 9. 
+
+
 // 10.
+
+
 
 int main() {
 
