03c9b62873fd87f7eb28baf45c527708d26a6e34
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-09-08 13:11:54 +0530

    Add : Problems 6 to 8 in BST

--- 10-Binary-Search-Tree/problems-6-to-10.cpp
+++ 10-Binary-Search-Tree/problems-6-to-10.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
 struct TreeNode {
@@ -29,13 +31,71 @@ int kthSmallest(TreeNode* root, int &k) {
 }
 
 
-// 7.
+// 7. Construct Special BST using Inorder
+void inOrderTraversal(TreeNode* root) {
+    if (!root) return ;
+    inOrderTraversal(root -> left);
+    cout << root -> val << " ";
+    inOrderTraversal(root -> right);
+}
+
+void preOrderTraversal(TreeNode* root) {
+    if (!root) return ;
+    cout << root -> val << " ";
+    preOrderTraversal(root -> left);
+    preOrderTraversal(root -> right);
+}
+
+int getMaxIndex(vector<int>& inorder, int &leftIndex, int &rightIndex) {
+    int index = 0;
+    for (int i=leftIndex; i<=rightIndex; i++) {
+        index = inorder[i] > inorder[index] ? i : index;
+    } 
+    return index;
+}
+
+TreeNode* bstHelper(vector<int>& inorder, int leftIndex, int rightIndex) {
+    // Base Case
+    if (leftIndex > rightIndex) return NULL;
+    
+    // Ek Case
+    int maxIndex = getMaxIndex(inorder, leftIndex, rightIndex);
+    TreeNode* root = new TreeNode(inorder[maxIndex]);
+
+    root -> left = bstHelper(inorder, leftIndex, maxIndex - 1);
+    root -> right = bstHelper(inorder, maxIndex + 1, rightIndex);
+
+    return root;
+}
+
+TreeNode* constructBST(vector<int> inorder) {
+    return bstHelper(inorder, 0, inorder.size()-1);
+}
 
 
-// 8.
+// 8. Two Sum in BST
+bool ftHelper(TreeNode* root, int &k, unordered_map<int, bool> &mp) {
+    // Base Case
+    if (!root) return false;
+
+    // Ek Case: Add every node value in mp
+    if (mp[k - root -> val]) return true;
+    mp[root -> val] = 1;
+
+    // Recursive Case: Traverse the tree
+    bool left = ftHelper(root -> left, k, mp);
+    bool right = ftHelper(root -> right, k, mp);
+
+    return left || right; 
+}
+
+bool findTarget(TreeNode* root, int k) {
+    unordered_map<int, bool> mp;
+    return ftHelper(root, k, mp);
+}
 
 
-// 9.
+// 9. 
 
 
 // 10.
@@ -44,7 +104,17 @@ int kthSmallest(TreeNode* root, int &k) {
 
 int main() {
 
+    vector<int> inorder{5, 10, 40, 30, 28};
     
+    TreeNode* root = constructBST(inorder);
+    
+    cout << "Inorder Traversal: ";
+    inOrderTraversal(root);
+    cout << endl;
+
+    cout << "Preorder Traversal: ";
+    preOrderTraversal(root);
+    cout << endl;
 
     return 0;
 }
