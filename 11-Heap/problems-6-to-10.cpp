be42184e6932de3474147b6a6fc2d7a2912aa24f
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-09-19 10:56:43 +0530

    Add : Problems 8 to 10 for Heap

--- 11-Heap/problems-6-to-10.cpp
+++ 11-Heap/problems-6-to-10.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <unordered_map>
 using namespace std;
 
 
@@ -59,13 +60,113 @@ ListNode* mergeKLists(vector<ListNode*>& lists) {
 // 7.
 
 
-// 8.
+// 8. Remove stones to minimize the total
+int minStoneSum(vector<int>& piles, int k) {
+    // Add all piles of stone in maxHeap
+    priority_queue<int> maxHeap;
+    for (auto p: piles) maxHeap.push(p);
+
+    // For k iterations, remove piles/2 where pile is the current largest pile in heap and add it back in heap
+    while (k--) {
+        int currPile = maxHeap.top(); maxHeap.pop();
+        currPile -= (currPile / 2);
+        maxHeap.push(currPile);
+    }
+
+    // Add all the piles in a variable and return it
+    int ans = 0;
+    while (!maxHeap.empty()) {
+        ans += maxHeap.top(); maxHeap.pop();
+    }
+
+    return ans;
+}
 
 
-// 9.
+// 9. Reorganize String
+string reorganizeString(string s) {
+    // Get frequency of each character
+    unordered_map<char, int> freq;
+    for (auto ch: s) freq[ch]++;
+
+    // Add them to max heap
+    priority_queue<pair<int, char>> maxHeap;
+    for (auto item: freq) {
+        maxHeap.push({item.second, item.first});
+    }
+
+    string ans;
+
+    // Edge case: If a character has frequency more than half of the total s size then reorganize not possible
+    if (!maxHeap.empty() && maxHeap.top().first > (s.size() / 2 + s.size() % 2)) return ans;
+
+    // Always pop 2 elements together and use them in answer string
+    while (maxHeap.size() > 1) {
+        // Pop top 2 freq characters 
+        auto top1 = maxHeap.top(); maxHeap.pop();
+        auto top2 = maxHeap.top(); maxHeap.pop();
+
+        // Add both characters in answer
+        ans += top1.second;
+        ans += top2.second;
+
+        // Decrement frequency after using it in main answer
+        top1.first--;
+        top2.first--;
+
+        // If frequency greater than 1 insert them back in heap
+        if (top1.first > 0) maxHeap.push(top1);
+        if (top2.first > 0) maxHeap.push(top2); 
+    }
+
+    // Add last remaining element in final answer
+    if (!maxHeap.empty()) ans += maxHeap.top().second;
+
+    return ans;
+}
 
 
-// 10.
+// 10. Longest Happy String
+string longestDiverseString(int a, int b, int c) {
+    // Push a, b and c in maxHeap
+    priority_queue<pair<int, char>> maxHeap;
+    if (a > 0) maxHeap.push({a, 'a'});
+    if (b > 0) maxHeap.push({b, 'b'});
+    if (c > 0) maxHeap.push({c, 'c'});
+
+    // Start building happy string
+    string ans;
+
+    while (maxHeap.size() > 1) {
+        // Pop top 2 elements from heap 
+        auto top1 = maxHeap.top(); maxHeap.pop();
+        auto top2 = maxHeap.top(); maxHeap.pop();
+
+        // Add top element
+        ans += top1.second;
+        top1.first--;
+
+        // If top1 element still has more frequency than top2 then add both elements once 
+        if (top1.first > top2.first) {
+            ans += top1.second;
+            ans += top2.second;
+            top1.first--;
+            top2.first--;
+        }
+
+        // Add back elements in heap whose frequencies are still over 0
+        if (top1.first > 0) maxHeap.push(top1);
+        if (top2.first > 0) maxHeap.push(top2);
+    }
+    
+    // Check if heap is not empty and the last heap element is not at the end of the ans string, if yes then add it to our answer
+    if ((ans.empty()) || (!maxHeap.empty() && maxHeap.top().second != ans.back())) {
+        ans += maxHeap.top().second;
+        if (maxHeap.top().first - 1 > 0) ans += maxHeap.top().second;
+    }
+
+    return ans;
+}
 
 
 
