4da36d5246bd563966a9bfc27b5d7bf28e0cd29b
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-10-03 13:53:51 +0530

    Add : Problems 14 and 15 for Heap

--- 11-Heap/problems-11-to-15.cpp
+++ 11-Heap/problems-11-to-15.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 using namespace std;
 
 // 11. Find Median from the data stream
@@ -174,11 +175,140 @@ vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
 }
 
 
-// 14.
+// 14. Sliding Window Maximum
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    // Use MaxHeap to keep track of maximum element in current window
+    // {element, index}
+    priority_queue<pair<int, int>> maxHeap;
+
+    // 1. Process first k elements
+    for (int i=0; i<k; i++) {
+        maxHeap.push({nums[i], i});
+    }
+
+    vector<int> ans;
+
+    // 2. Get answer for first window
+    ans.push_back(maxHeap.top().first);
+
+    // 3. Process remaining windows
+    for (int i=k; i<nums.size(); i++) {
+        // Remove elements that are out of range of current window
+        while (!maxHeap.empty() && maxHeap.top().second <= i - k) maxHeap.pop();
+
+        // Add current element in our window
+        maxHeap.push({nums[i], i});
+
+        // Update answer array for current window
+        ans.push_back(maxHeap.top().first);
+    }
+
+    return ans;
+}
 
 
-// 15.
+// 15. Get biggest 3 rhombus
+bool validRhombus(pair<int,int> &up, pair<int,int> &right, pair<int,int> &down, pair<int,int> &left, int &row, int &col) {
+    // Check up
+    if (up.first < 0 || up.first >= row || up.second < 0 || up.second >= col) return false;
+    // Check right
+    if (right.first < 0 || right.first >= row || right.second < 0 || right.second >= col) return false;
+    // Check down
+    if (down.first < 0 || down.first >= row || down.second < 0 || down.second >= col) return false;
+    // Check left
+    if (left.first < 0 || left.first >= row || left.second < 0 || left.second >= col) return false;
 
+    return true;
+}
+
+void expandRhombus(vector<vector<int>>& grid, int currRow, int currCol, int &rowSize, int &colSize, priority_queue<int> &maxHeap) {
+    // Create 4 vertices that represents four corner points of a rhombus
+    pair<int, int> up = make_pair(currRow-1, currCol);          // Expands to the top
+    pair<int, int> right = make_pair(currRow, currCol+1);       // Expands to the right
+    pair<int, int> down = make_pair(currRow+1, currCol);        // Expands to the bottom
+    pair<int, int> left = make_pair(currRow, currCol-1);        // Expands to the left
+
+    while (validRhombus(up, right, down, left, rowSize, colSize)) {
+        // Calculate Rhombus Sum
+        // A : Add vertices
+        int sum = grid[up.first][up.second] + grid[right.first][right.second] + grid[down.first][down.second] + grid[left.first][left.second];
+        // B : Add edges
+        int x, y;
+        // Traverse UP to RIGHT
+        x = up.first + 1;
+        y = up.second + 1;
+
+        while (x != right.first && y != right.second) {
+            sum += grid[x++][y++];
+        }
+
+        // Traverse RIGHT to DOWN
+        x = right.first + 1;
+        y = right.second - 1;
+
+        while (x != down.first && y != down.second) {
+            sum += grid[x++][y--];
+        }
+
+        // Traverse DOWN to LEFT
+        x = down.first - 1;
+        y = down.second - 1;
+
+        while (x != left.first && y != left.second) {
+            sum += grid[x--][y--];
+        }
+
+        // Traverse LEFT to UP
+        x = left.first - 1;
+        y = left.second + 1;
+
+        while (x != up.first && y != up.second) {
+            sum += grid[x--][y++];
+        }
+
+        // Expand Rhombus
+        up.first--;
+        right.second++;
+        down.first++;
+        left.second--;
+
+        // Add sum to heap
+        maxHeap.push(sum);
+    }
+}
+
+vector<int> getBiggestThree(vector<vector<int>>& grid) {
+    // 1. Create a MaxHeap to hold rhombus sums
+    priority_queue<int> maxHeap;
+
+    // 2. Loop through the grid
+    int row = grid.size(), col = grid[0].size();
+    for (int r=0; r<grid.size(); r++) {
+        for (int c=0; c<grid[r].size(); c++) {
+            // 3. Consider the single cell as a rhombus and add it to heap as its sum
+            maxHeap.push(grid[r][c]);
+            // 4. Expand rhombus
+            expandRhombus(grid, r, c, row, col, maxHeap);
+        }
+    }
+
+    // 3. Add top 3 answers in array
+    vector<int> ans;
+
+    while (!maxHeap.empty() && ans.size() < 3) {
+        int top = maxHeap.top();
+
+        if (ans.size() > 0 && ans.back() == top) {
+            maxHeap.pop();
+        }
+        else {
+            ans.push_back(top);
+            maxHeap.pop();
+        }
+    }
+
+    return ans;
+}
 
 
 int main() {
