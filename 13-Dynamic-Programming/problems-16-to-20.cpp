90b536e0597313cca29a2f4014b43b66477067cb
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-11-06 11:14:30 +0530

    Add : Buy and Sell stocks 2 problem using Recursion, Memoization, Tabulation and Space Optimization

--- 13-Dynamic-Programming/problems-16-to-20.cpp
+++ 13-Dynamic-Programming/problems-16-to-20.cpp
@@ -181,7 +181,127 @@ int mincostTickets(vector<int>& days, vector<int>& costs) {
 }
 
 
-// 18.
+// 18. Buy and Sell Stocks 2
+int mpRec(vector<int>& prices, int i, bool isHolding) {
+    // Base Case
+    if (i >= prices.size()) return 0;
+
+    // Ek Case
+    // Buy stock
+    int buyResult = 0, sellResult = 0;
+    if (!isHolding) {
+        int buy = mpRec(prices, i + 1, true) - prices[i];
+        int skip = mpRec(prices, i + 1, isHolding);
+        buyResult = max(buy, skip);
+    }
+    // Sell Stock
+    else {
+        int sell = mpRec(prices, i + 1, false) + prices[i];
+        int skip = mpRec(prices, i + 1, isHolding);
+        sellResult = max(sell, skip);
+    }
+
+    return max(buyResult, sellResult); 
+}
+
+int mpMem(vector<int>& prices, int i, bool isHolding, vector<vector<int>>& dp) {
+    // Base Case
+    if (i >= prices.size()) return 0;
+
+    // DP Case
+    if (dp[i][isHolding] != -1) return dp[i][isHolding];
+
+    // Ek Case
+    int buyResult = 0, sellResult = 0;
+    if (!isHolding) {
+        int buy = mpMem(prices, i + 1, true, dp) - prices[i];
+        int skip = mpMem(prices, i + 1, isHolding, dp);
+        buyResult = max(buy, skip);
+    }
+    // Sell Stock
+    else {
+        int sell = mpMem(prices, i + 1, false, dp) + prices[i];
+        int skip = mpMem(prices, i + 1, isHolding, dp);
+        sellResult = max(sell, skip);
+    }
+
+    dp[i][isHolding] = max(buyResult, sellResult);
+    return dp[i][isHolding];
+}
+
+int mpTab(vector<int>& prices) {
+    // Step 1: DP Array
+    vector<vector<int>> dp(prices.size() + 1, vector<int>(2, 0));
+    // Step 2: Base Case addition
+
+    // Step 3: Bottom Up
+    for (int i=prices.size()-1; i>=0; i--) {
+        for (int isHolding=0; isHolding<=1; isHolding++) {
+            int buyResult = 0, sellResult = 0;
+            if (!isHolding) {
+                int buy = dp[i + 1][true] - prices[i];
+                int skip = dp[i + 1][isHolding];
+                buyResult = max(buy, skip);
+            }
+            // Sell Stock
+            else {
+                int sell = dp[i + 1][false] + prices[i];
+                int skip = dp[i + 1][isHolding];
+                sellResult = max(sell, skip);
+            }
+
+            dp[i][isHolding] = max(buyResult, sellResult);
+        }
+    }
+
+    return dp[0][0];
+}
+
+int mpSO(vector<int>& prices) {
+    // Step 1: DP Array
+    vector<int> currRow(2, 0);
+    vector<int> nextRow(2, 0);
+    // Step 2: Base Case addition
+
+    // Step 3: Bottom Up
+    for (int i=prices.size()-1; i>=0; i--) {
+        for (int isHolding=0; isHolding<=1; isHolding++) {
+            int buyResult = 0, sellResult = 0;
+            if (!isHolding) {
+                int buy = nextRow[true] - prices[i];
+                int skip = nextRow[isHolding];
+                buyResult = max(buy, skip);
+            }
+            // Sell Stock
+            else {
+                int sell = nextRow[false] + prices[i];
+                int skip = nextRow[isHolding];
+                sellResult = max(sell, skip);
+            }
+
+            currRow[isHolding] = max(buyResult, sellResult);
+        }
+
+        nextRow = currRow;
+    }
+
+    return currRow[0];
+}
+
+
+int maxProfit(vector<int>& prices) {
+    // Recursion Solution
+    /*
+    return mpRec(prices, 0, false);
+    */
+    // Memoization Solution
+    /*
+    vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
+    return mpMem(prices, 0, false, dp);
+    */
+    // Tabulation Solution
+    return mpTab(prices);
+}
 
 
 // 19.
