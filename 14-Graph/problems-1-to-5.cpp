cc54eada5304670239cde41fa1c5d74b93825c8e
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-12-21 13:30:43 +0530

    Add : Number of islands and Flood fill problem solutions using DFS

--- 14-Graph/problems-1-to-5.cpp
+++ 14-Graph/problems-1-to-5.cpp
@@ -39,10 +39,67 @@ int findCircleNum(vector<vector<int>>& isConnected) {
 }
 
 
-// 2.
+// 2. Number of Islands
+void dfs(vector<vector<char>>& grid, int i, int j, vector<vector<bool>>& vis) {
+    // Visit current cell
+    vis[i][j] = 1;
+
+    // Visit neighbours in 4 directions to make current island visited
+    // Top
+    if (i - 1 >= 0 && grid[i - 1][j] == '1' && !vis[i - 1][j]) dfs(grid, i - 1, j, vis);
+    // Right
+    if (j + 1 < grid[0].size() && grid[i][j + 1] == '1' && !vis[i][j + 1]) dfs(grid, i, j + 1, vis);
+    // Bottom
+    if (i + 1 < grid.size() && grid[i + 1][j] == '1' && !vis[i + 1][j]) dfs(grid, i + 1, j, vis);
+    // Left
+    if (j - 1 >= 0 && grid[i][j - 1] == '1' && !vis[i][j - 1]) dfs(grid, i, j - 1, vis);
+}
+
+int numIslands(vector<vector<char>>& grid) {
+    vector<vector<bool>> vis(grid.size(), vector<bool>(grid[0].size(), false));
+    int island = 0;
+
+    // Loop through and visit each island one at a time
+    for (int i=0; i<grid.size(); i++) {
+        for (int j=0; j<grid[i].size(); j++) {
+            if (grid[i][j] == '1' && !vis[i][j]) {
+                island++;
+                dfs(grid, i, j, vis);
+            }
+        }
+    }
+
+    return island;
+}
 
 
-// 3.
+// 3. Flood Fill
+void fillHelper(vector<vector<int>>& img, int sr, int sc, int &color,vector<vector<bool>>& vis, int &ogColor) {
+    // Fill current cell with color
+    img[sr][sc] = color;
+    vis[sr][sc] = 1;
+    // Visit neighbours of the same color to fill it with color
+    // Top
+    if (sr - 1 >= 0 && img[sr - 1][sc] == ogColor && !vis[sr - 1][sc]) 
+        fillHelper(img, sr - 1, sc, color, vis, ogColor);
+    // Right
+    if (sc + 1 < img[0].size() && img[sr][sc + 1] == ogColor && !vis[sr][sc + 1]) 
+        fillHelper(img, sr, sc + 1, color, vis, ogColor);
+    // Bottom
+    if (sr + 1 < img.size() && img[sr + 1][sc] == ogColor && !vis[sr + 1][sc])
+        fillHelper(img, sr + 1, sc, color, vis, ogColor);
+    // Left
+    if (sc - 1 >= 0 && img[sr][sc - 1] == ogColor && !vis[sr][sc - 1])
+        fillHelper(img, sr, sc - 1, color, vis, ogColor);
+
+}
+
+vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+    vector<vector<bool>> vis(image.size(), vector<bool>(image[0].size(), 0));
+    int ogColor = image[sr][sc];
+    fillHelper(image, sr, sc, color, vis, ogColor);
+    return image;
+}
 
 
 // 4.
