1c94006c9f38312727e6e73cb2f72a391ae3fbdd
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-12-23 15:49:17 +0530

    Add : Course Schedule problem solution using BFS

--- 14-Graph/problems-1-to-5.cpp
+++ 14-Graph/problems-1-to-5.cpp
@@ -168,7 +168,50 @@ int orangesRotting(vector<vector<int>>& grid) {
 }
 
 
-// 5.
+// 5. Course Schedule
+vector<int> topologicalSort(unordered_map<int, list<int>> &adjList, int &numCourses) {
+    vector<int> topoSort;
+    // Step 1: Create Indegree of each node
+    unordered_map<int, int> indegree;
+    for (auto it: adjList) {
+        int node = it.first;
+        for (auto nbr: it.second) {
+            indegree[nbr]++;
+        }
+    }
+
+    // Step 2: Insert all independent nodes in queue for BFS
+    queue<int> indNode;
+
+    for (int i=0; i<numCourses; i++) {
+        if (indegree[i] == 0) indNode.push(i);
+    }
+    
+    // Step 3: Use BFS to get all courses that can be completed
+    while (!indNode.empty()) {
+        int node = indNode.front(); indNode.pop();
+        topoSort.push_back(node);
+
+        for (auto nbr: adjList[node]) {
+            indegree[nbr]--;
+            if (indegree[nbr] == 0) indNode.push(nbr);
+        }
+    }
+
+    return topoSort;
+}
+
+bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    // Create an adjacency list
+    unordered_map<int, list<int>> adjList;
+    for (auto it: prerequisites) {
+        adjList[it[1]].push_back(it[0]);
+    }
+    
+    // Get the topological sort of courses graph
+    vector<int> ans = topologicalSort(adjList, numCourses);
+    return ans.size() == numCourses;
+}
 
 
 int main() {
